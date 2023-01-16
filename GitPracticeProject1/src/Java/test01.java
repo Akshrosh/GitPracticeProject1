a70cd655111590775e4b815aa6ac6b1624c0ce64
Akshay4394 <Akshaypatel4394@gmail.com>
2023-01-15 20:07:51 -0600

    Created Project transferring into Git

---
+++ GitPracticeProject1/src/Java/test01.java
@@ -0,0 +1,22 @@
+package Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		
+      List<Integer> lst = new ArrayList<>();
+      
+     
+      lst.add(0,12);
+      lst.add(1,20);
+      lst.add(2,14);
+      
+      System.out.println(lst.get(0));
+     
+      
+	}
+
+}
